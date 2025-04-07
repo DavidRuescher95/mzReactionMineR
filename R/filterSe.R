dc93ec71410cba7b78bdcd927e9570ffd1c5fd54
Rüscher <druesche@ipb-halle.de>
2025-04-07 09:24:49 +0200

    add filter SE.R

---
+++ R/filterSe.R
@@ -0,0 +1,138 @@
+#' filterSE
+#'
+#' A function that filters a SummarizedExperiment object based on various
+#'     measures.
+#'
+#' @importFrom dplyr %>% mutate filter group_by summarize ungroup pull sym n
+#'     inner_join
+#' @importFrom tidyr pivot_longer
+#' @importFrom SummarizedExperiment SummarizedExperiment rowData assays colData
+#' @importFrom tidyselect all_of
+#'
+#' @param object A SummarizedExperiment object
+#' @param assay Assay name to filter on
+#' @param sample_col Character. Name of the column in the colData that
+#'     that corresponds to the assay columns
+#' @param group_col Character. Either "none" or a columnname in the colData
+#' @param not_in Character. Either "none" or a value in the group_col.
+#'     Specify, if an id should not be present in a group.
+#' @param min_abundance Numeric. Minimum abundance for a feature to be present
+#'     in a sample
+#' @param min_pct Numeric. Minimum percentage of samples a feature must be
+#'     present in. If group_col is specified, the percentage is calculated for
+#'     for each group.
+#' @param mz_range Numeric. A vector of length 2. The range of kept m/z values.
+#' @param rt_range Numeric. A vector of length 2. The range of kept rt values.
+#' @param mobility_range Numeric. A vector of length 2.
+#'     The range of kept ion mobility values.
+#' @param id_col Character. The respective column name in the rowData.
+#' @param rt_col Character. The respective column name in the rowData.
+#' @param mz_col Character. The respective column name in the rowData.
+#' @param ion_mobility_col Character. The respective column name in the rowData.
+#'
+#' @returns A filtered summarizeExperiment object
+#' @export
+filterSe <- function(
+  object = NULL,
+  assay = NULL,
+  sample_col = "filename",
+  group_col = "none",
+  not_in = "none",
+  min_abundance = 0,
+  min_pct = 0.8,
+  mz_range = c(0,Inf),
+  rt_range = c(0,Inf),
+  mobility_range = c(0,Inf),
+  id_col = "id",
+  rt_col = "rt",
+  mz_col = "mz",
+  ion_mobility_col = "ion_mobility"
+) {
+
+  # get column names of rowData
+
+  id_cols <- names(as.data.frame(rowData(object)))
+
+  # make long data frame of assay for easy filtering
+
+  data_long <- cbind(
+    as.data.frame(rowData(object)),
+    as.data.frame(assays(object)[[assay]]) %>%
+      replace(is.na(.data),0) %>%
+      replace(.data > min_abundance, 1)
+  ) %>%
+    pivot_longer(
+      cols = -all_of(id_cols),
+      names_to = sample_col,
+      values_to = "Value"
+    ) %>%
+    inner_join(
+      as.data.frame(colData(object))
+    ) %>%
+    filter(
+      !!sym(mz_col) > mz_range[1] & !!sym(mz_col) < mz_range[2],
+      !!sym(rt_col) > rt_range[1] & !!sym(rt_col) < rt_range[2],
+      !!sym(ion_mobility_col) > mobility_range[1] &
+        !!sym(ion_mobility_col) < mobility_range[2]
+    )
+
+  # remove features with values < min_pct
+
+  if(group_col != "none") {
+
+    if(not_in != "none") {
+
+      ids <- data_long %>%
+        filter(
+          !!sym(group_col) %in% not_in
+        ) %>%
+        group_by(!!sym(id_col)) %>%
+        summarize(
+          Value = sum(.data$Value)
+        ) %>%
+        filter(
+          Value == 0
+        ) %>%
+        ungroup() %>%
+        pull(!!sym(id_col)) %>%
+        unique()
+
+      data_long_grouped <- data_long %>%
+        filter(
+          !!sym(id_col) %in% ids
+        ) %>%
+        group_by(!!sym(id_col), !!sym(group_col))
+
+    } else {
+
+      data_long_grouped <- data_long %>%
+        group_by(!!sym(id_col), !!sym(group_col))
+
+    }
+
+  } else {
+
+    data_long_grouped <- data_long %>%
+      group_by(!!sym(id_col))
+
+  }
+
+  ids <- data_long_grouped %>%
+    summarize(
+      Value = sum(.data$Value),
+      cutoff = floor(n()*min_pct)
+    ) %>%
+    filter(
+      Value >= .data$cutoff
+    ) %>%
+    ungroup() %>%
+    pull(!!sym(id_col)) %>%
+    unique()
+
+  result <- object[rowData(object)[[id_col]] %in% ids,]
+
+  return(result)
+
+}
+
+utils::globalVariables(c(".",".data"))
--- R/mzmine_to_se.R
+++ R/mzmine_to_se.R
@@ -1,16 +1,19 @@
-#' Title
+#' mzmine_to_se
+#'
+#' Convert mzMine feature table to a SummarizedExperiment object. Assumes the
+#'     "Export to CSV (modular)" was used
+#'
 #' @importFrom utils read.csv
 #' @importFrom dplyr %>% mutate group_by
 #' @importFrom SummarizedExperiment SummarizedExperiment assays rowData colData
-#'     assays<-
-#' @importFrom limma normalizeVSN
 #' @param path_to_file path to the mzmine feature table
-#' @param path_to__meta_data data.frame with sample meta data
+#' @param sample_meta_data data.frame. sample meta data to become colData
 #'     the first columnn has to be the sample names
-#' @param intensity_type either "area" or "height"
-#' @param normalize logical. Whether to normalize the data
-#' @param transform logical. Whether to transform the normalized data
-#' @param vsn logical. Whether to perform variance stabilizing normalization
+#' @param intensity_type character. either "area" or "height"
+#' @param rowData_cols character vector. column names to be used as rowData
+#' @param remove_adducts logical. if TRUE, remove adducts from the feature
+#'     table. Requires that the ion_identities.iin_id column is present in the
+#'     data.
 #'
 #' @returns a summarizedExperiment Object
 #' @export
@@ -20,9 +23,8 @@ mzmine_to_se <- function(
     path_to_file,
     sample_meta_data,
     intensity_type = c("area","height"),
-    normalize = TRUE,
-    transform = TRUE,
-    vsn = TRUE
+    rowData_cols = c("id","rt","mz","ion_mobility"),
+    remove_adducts = FALSE
 ) {
 
   # load feature data
@@ -40,6 +42,10 @@ mzmine_to_se <- function(
 
   print("Processing feature table")
 
+  if(remove_adducts) {
+    features <- features[is.na(features$ion_identities.iin_id), ]
+  }
+
   feature_area <- features[,names(features)[grepl(paste0("[.]",intensity_type),names(features))]]
 
   # rename to fit the file names in meta_data
@@ -49,44 +55,25 @@ mzmine_to_se <- function(
 
   sample_meta_data[,1] <- make.names(sample_meta_data[,1])
 
+  samples <- intersect(
+    names(feature_area),
+    sample_meta_data[,1]
+  )
+
+
   # generate summarized experiment
 
   print("Creating SummarizedExperiment object")
 
   se <- SummarizedExperiment(
-    rowData = features[,c("id","rt","mz","ion_mobility")],
+    rowData = features[,rowData_cols],
     assays = list(
-      raw = as.matrix(feature_area)
+      raw = as.matrix(feature_area[,samples])
     ),
     colData = sample_meta_data[
-      match(names(feature_area),sample_meta_data[,1]),
+      match(samples, sample_meta_data[,1]),
     ]
   )
-
-  # processing
-
-  if (normalize) {
-
-    print("Normalizing data")
-
-    assays(se)$normalized <- normalizePQN(assays(se)$raw,"median")
-  }
-
-
-  if (transform) {
-
-    print("log2(x+1) transformation of normalized data")
-
-    assays(se)$transformed <- log2(assays(se)$normalized + 1)
-  }
-
-  if(vsn) {
-
-    print("Variance stabilizing normalization of raw data")
-
-    assays(se)$vsn <- normalizeVSN(assays(se)$transformed)
-
-  }
 
   return(se)
 
